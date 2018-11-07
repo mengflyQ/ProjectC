2f8b68a5947a4fa751b7fc9ea6d8ec0ca695f009
zd304 <568892758@qq.com>
2018-11-08 01:30:16 +0800

    init server

---
+++ Server/Script/CsScript/Action/Action100.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZyGames.Framework.Game.Service;
+
+namespace GameServer.CsScript.Action
+{
+    public class Action100 : BaseStruct
+    {
+        public Action100(ActionGetter httpGet)
+            : base(100, httpGet)
+        {
+
+        }
+
+        public override bool GetUrlElement()
+        {
+            return base.GetUrlElement();
+        }
+
+        public override bool TakeAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void BuildPacket()
+        {
+            base.BuildPacket();
+        }
+    }
+}
