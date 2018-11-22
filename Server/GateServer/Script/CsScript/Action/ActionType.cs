96ffc9f7a75e2d5133dc6bbe7feaa9e0ac68aa7a
zd304 <568892758@qq.com>
2018-11-22 15:30:05 +0800

    GateServer

---
+++ Server/GateServer/Script/CsScript/Action/ActionType.cs
@@ -0,0 +1,7 @@
+﻿namespace GameServer.CsScript.Action
+{
+    public enum ActionType
+    {
+        Login = 1001,
+    }
+}
---
+++ Server/GateServer/Script/Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Script.Model")]
+[assembly: AssemblyProduct("Script.Model")]
+[assembly: AssemblyCopyright("Copyright Scut")]
+[assembly: ComVisible(false)]
+[assembly: Guid("75393df8-4731-48ce-83d4-060f1f8925f7")]
+[assembly: AssemblyVersion("1.0.0.1")]
