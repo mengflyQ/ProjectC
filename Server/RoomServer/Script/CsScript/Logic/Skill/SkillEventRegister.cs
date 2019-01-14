91f99f53f1e86274cc054c5b76139d48236c4dd2
zd304 <568892758@qq.com>
2019-01-14 17:03:22 +0800

    朝向目标位置事件

--- Server/RoomServer/Script/CsScript/Logic/Skill/SkillEventRegister.cs
+++ Server/RoomServer/Script/CsScript/Logic/Skill/SkillEventRegister.cs
@@ -35,10 +35,27 @@ public static class SkillEventRegister
         }
     }
 
+    static void TowardTargetPos(Character cha, SkillContext context, excel_skill_event e)
+    {
+        Vector3 ownerPos = Vector3.zero;
+        if (cha != null)
+            ownerPos = cha.Position;
+        //else if (childObject != null)
+        //    ownerPos = childObject.Position;
+        else
+            return;
+        Vector3 dir = context.TargetPos - ownerPos;
+        if (cha != null)
+            cha.Direction = dir;
+        //else if (childObject != null)
+        //    childObject.Direction = dir;
+    }
+
     public static void Initialize()
     {
         events[SkillEventType.Hit] = Hit;
         events[SkillEventType.SkillMove] = SkillMove;
+        events[SkillEventType.TowardTargetPos] = TowardTargetPos;
     }
     public delegate void SkillEventMethod(Character cha, SkillContext context, excel_skill_event e);
     public static Dictionary<SkillEventType, SkillEventMethod> events = new Dictionary<SkillEventType, SkillEventMethod>();
